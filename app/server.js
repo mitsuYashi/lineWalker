fb35f591068670e1078733f92a085a29d2b785f5
mitsuYashi <hm20011027@gmail.com>
2022-08-15 13:58:33 +0900

    fix: test

--- app/server.js
+++ app/server.js
@@ -1,22 +1,39 @@
-// -----------------------------------------------------------------------------
-// モジュールのインポート
-const server = require("express")();
-const line = require("@line/bot-sdk"); // Messaging APIのSDKをインポート
+const express = require("express");
+const app = express();
+const line = require("@line/bot-sdk");
+const PORT = process.env.PORT || 3000;
 
-// -----------------------------------------------------------------------------
-// パラメータ設定
 const line_config = {
-  channelAccessToken: process.env.LINE_ACCESS_TOKEN, // 環境変数からアクセストークンをセットしています
-  channelSecret: process.env.LINE_CHANNEL_SECRET, // 環境変数からChannel Secretをセットしています
+  channelAccessToken: process.env.LINE_ACCESS_TOKEN,
+  channelSecret: process.env.LINE_CHANNEL_SECRET,
 };
 
-// -----------------------------------------------------------------------------
-// Webサーバー設定
-server.listen(process.env.PORT || 3000);
+app.get("/", (req, res) => res.send("Hello LINE BOT!(GET)")); //ブラウザ確認用(無くても問題ない)
+// app.post("/webhook", line.middleware(line_config), (req, res) => {
+//   console.log(req.body.events);
+//   Promise.all(req.body.events.map(handleEvent)).then((result) =>
+//     res.json(result)
+//   );
+// });
 
-// -----------------------------------------------------------------------------
-// ルーター設定
-server.post("/bot/webhook", line.middleware(line_config), (req, res) => {
-  res.sendStatus(200);
-  console.log(req.body);
-});
+// const client = new line.Client(config);
+
+// async function handleEvent(event) {
+//   let msg = ``;
+
+//   if (event.type !== "message" || event.message.type !== "text") {
+//     return Promise.resolve(null);
+//   }
+
+//   const profile = await client.getProfile(event.source.userId);
+//   console.log(profile);
+//   msg = `${profile.displayName}さんこんにちは。 あなたのユーザーIDは${profile.userId}です。`;
+
+//   return client.replyMessage(event.replyToken, {
+//     type: "text",
+//     text: msg,
+//   });
+// }
+
+app.listen(PORT);
+console.log(`Server running at ${PORT}`);
