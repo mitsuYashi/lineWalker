f0ec49c78922dcd15bc055586bd6830d312b1999
mitsuYashi <hm20011027@gmail.com>
2022-08-24 11:32:37 +0900

    add: server.js

--- app/server.js
+++ app/server.js
@@ -1,55 +1,40 @@
-// require("dotenv").config();
-// const express = require("express");
-// const app = express();
-// const line = require("@line/bot-sdk");
-// const PORT = process.env.PORT || 3000;
-
-// const config = {
-//   channelAccessToken: process.env.LINE_ACCESS_TOKEN,
-//   channelSecret: process.env.LINE_CHANNEL_SECRET,
-// };
-
-// // console.log(process.env.LINE_CHANNEL_SECRET);
-
-// app.get("/", (req, res) => res.send("Hello LINE BOT!(GET)")); //ブラウザ確認用(無くても問題ない)
-// app.post("/webhook", line.middleware(config), (req, res) => {
-//   console.log(req.body.events);
-//   Promise.all(req.body.events.map(handleEvent)).then((result) =>
-//     res.json(result)
-//   );
-// });
-
-// const client = new line.Client(config);
-
-// async function handleEvent(event) {
-//   let msg = ``;
-
-//   if (event.type !== "message" || event.message.type !== "text") {
-//     return Promise.resolve(null);
-//   }
-
-//   const profile = await client.getProfile(event.source.userId);
-//   console.log(profile);
-//   msg = `${profile.displayName}さんこんにちは。 あなたのユーザーIDは${profile.userId}です。`;
-
-//   return client.replyMessage(event.replyToken, {
-//     type: "text",
-//     text: msg,
-//   });
-// }
-
-// app.listen(PORT);
-// console.log(`Server running at ${PORT}`);
-
-// ----------------------------------
+require("dotenv").config();
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
+const line = require("@line/bot-sdk");
+const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const config = {
+  channelAccessToken: process.env.LINE_ACCESS_TOKEN,
+  channelSecret: process.env.LINE_CHANNEL_SECRET,
+};
+
+app.get("/", (req, res) => res.send("Hello LINE BOT!(GET)"));
+app.post("/webhook", line.middleware(config), (req, res) => {
+  console.log(req.body.events);
+  Promise.all(req.body.events.map(handleEvent)).then((result) =>
+    res.json(result)
+  );
 });
 
-app.listen(port, () => {
-  console.log("Listening on port", port);
-});
+const client = new line.Client(config);
+
+async function handleEvent(event) {
+  let msg = ``;
+
+  if (event.type !== "message" || event.message.type !== "text") {
+    return Promise.resolve(null);
+  }
+
+  const profile = await client.getProfile(event.source.userId);
+  console.log(profile);
+  msg = `${profile.displayName}さんこんにちは。 あなたのユーザーIDは${profile.userId}です。`;
+
+  return client.replyMessage(event.replyToken, {
+    type: "text",
+    text: msg,
+  });
+}
+
+app.listen(PORT);
+console.log(`Server running at ${PORT}`);
