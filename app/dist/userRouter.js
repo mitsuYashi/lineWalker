4aa0a5a11483e9b5fb50307e423b567d19d6eb55
mitsuYashi <hm20011027@gmail.com>
2023-02-21 14:15:01 +0900

    fix: env

--- app/dist/userRouter.js
+++ app/dist/userRouter.js
@@ -6,6 +6,7 @@ import { google } from "googleapis";
 import { addDays, startOfDay } from "date-fns";
 import pkg from "date-fns-tz";
 const { utcToZonedTime } = pkg;
+const dev = process.env.NODE_ENV !== "production";
 const router = express.Router();
 router.use(session({
     secret: "secretkey",
@@ -19,12 +20,12 @@ router.use((req, res, next) => {
     // req.session.refresh_token = "";
     next();
 });
-router.get("/", (req, res, next) => {
-    const options = {
-        clientId: process.env.client_id,
-        clientSecret: process.env.client_secret,
-        redirectUri: "http://localhost:3000/user/oauth2callback",
-    };
+const options = {
+    clientId: process.env.client_id,
+    clientSecret: process.env.client_secret,
+    redirectUri: JSON.parse(process.env.redirect_uris ?? "")[dev ? 0 : 1],
+};
+router.get("/", (req, res) => {
     const oauth2Client = new google.auth.OAuth2(options);
     const scopes = [
         "https://www.googleapis.com/auth/fitness.location.read",
@@ -39,11 +40,11 @@ router.get("/", (req, res, next) => {
     res.end();
 });
 router.get("/oauth2callback", async (req, res) => {
-    const options = {
-        clientId: process.env.client_id,
-        clientSecret: process.env.client_secret,
-        redirectUri: "http://localhost:3000/user/oauth2callback",
-    };
+    // const options = {
+    //   clientId: process.env.client_id,
+    //   clientSecret: process.env.client_secret,
+    //   redirectUri: "http://localhost:8080/user/oauth2callback",
+    // };
     const oauth2Client = new google.auth.OAuth2(options);
     const code = req.query.code;
     const { tokens } = await oauth2Client.getToken(code);
@@ -58,11 +59,11 @@ router.get("/steps", async (req, res) => {
     if (!req.session.refresh_token) {
         return res.redirect("/user");
     }
-    const options = {
-        clientId: process.env.client_id,
-        clientSecret: process.env.client_secret,
-        redirectUri: "http://localhost:3000/user/oauth2callback",
-    };
+    // const options = {
+    //   clientId: process.env.client_id,
+    //   clientSecret: process.env.client_secret,
+    //   redirectUri: "http://localhost:8080/user/oauth2callback",
+    // };
     const oauth2Client = new google.auth.OAuth2(options);
     oauth2Client.setCredentials({
         refresh_token: req.session.refresh_token,
@@ -93,7 +94,7 @@ router.get("/steps", async (req, res) => {
         res.send(fitRes?.data?.bucket?.[0]?.dataset?.[0]?.point?.[0]?.value?.[0].intVal?.toString());
     }
     catch (err) {
-        res.status(500).send({ message: "AOuth2.0 denieded scope" });
+        res.status(500).send({ message: "permission denieded" });
     }
 });
 export default router;
--- app/src/server.ts
+++ app/src/server.ts
@@ -1,6 +1,6 @@
+import * as dotenv from "dotenv";
+dotenv.config();
 import express, { Request, Response } from "express";
-// import { createServer } from "http";
-// import { bodyParser } from "body-parser";
 import userRouter from "./userRouter.js";
 
 const dev = process.env.NODE_ENV !== "production";
